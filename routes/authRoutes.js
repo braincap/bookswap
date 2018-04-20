03c30aee06a33387843f84e95ff53826579f2558
braincap <contactjaitiwari@gmail.com>
2018-04-20 17:27:59 -0400

    final

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,6 +15,11 @@ module.exports = app => {
     (req, res) => res.redirect('/')
   );
 
+  app.get('/test', (req, res) => {
+    console.log('test');
+    res.send('LOL');
+  });
+
   app.get('/api/logout', (req, res) => {
     console.log('2) JT LOGOUT ROUTE');
     req.logout();
